9c2921a68e989d046bfd925e7b3c2d227b48c9e4
prapanna2468 <161803920+prapanna2468@users.noreply.github.com>
2025-07-05 10:21:28 +0545

    Update RegisterController.java

--- src/main/java/com/tourism/controllers/RegisterController.java
+++ src/main/java/com/tourism/controllers/RegisterController.java
@@ -1,5 +1,6 @@
 package com.tourism.controllers;
 
+import com.tourism.Main;
 import com.tourism.models.*;
 import com.tourism.utils.FileHandler;
 import com.tourism.utils.LanguageManager;
@@ -226,14 +227,10 @@ public class RegisterController {
     private void handleBack() {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
-            Scene scene = new Scene(loader.load(), 900, 700);
+            Scene scene = new Scene(loader.load());
             
-            Stage stage = (Stage) backButton.getScene().getWindow();
-            stage.setTitle("Nepal Tourism Management System");
-            stage.setScene(scene);
-            
-            // Restore proper window state
-            stage.setMaximized(true);
+            // Use the new scene switching method to maintain full screen
+            Main.switchScene(scene, "Journey - Nepal Tourism System");
             
         } catch (Exception e) {
             e.printStackTrace();
